6109733e320d80276ebb3a1c8fc1f097a6a58b04
Oliver Zarate <ozarate@cloudera.com>
2024-06-25 20:17:21 +0000

    latest update

--- intall_packages.R
+++ intall_packages.R
@@ -1,23 +1,5 @@
-# Check the version of the vctrs package
 install.packages("sparklyr")
 install.packages("lubridate")
 install.packages("dplyr")
 install.packages("forecast")
 install.packages("arrow")
-
-# Now, try loading the 'sparklyr' package again
-install.packages("vctrs")
-
-
-#vctrs_version <- packageVersion("vctrs")
-#print(vctrs_version)
-
-# Ensure the 'remotes' package is installed
-#if (!requireNamespace("remotes", quietly = TRUE)) {
-#  install.packages("remotes")
-#}
-## Install a specific version of 'vctrs'
-#remotes::install_version("vctrs", version = "0.6.4")
-
-
-
