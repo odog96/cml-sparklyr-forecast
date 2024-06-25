e1cac294d39ec6bbcaf7af2257db899a58828e13
Oliver Zarate <ozarate@cloudera.com>
2024-06-25 16:38:32 +0000

    updated install packages and added yaml file

--- intall_packages.R
+++ intall_packages.R
@@ -5,17 +5,19 @@ install.packages("dplyr")
 install.packages("forecast")
 install.packages("arrow")
 
+# Now, try loading the 'sparklyr' package again
+install.packages("vctrs")
+
+
 #vctrs_version <- packageVersion("vctrs")
 #print(vctrs_version)
 
 # Ensure the 'remotes' package is installed
-if (!requireNamespace("remotes", quietly = TRUE)) {
-  install.packages("remotes")
-}
-# Install a specific version of 'vctrs'
-remotes::install_version("vctrs", version = "0.6.4")
+#if (!requireNamespace("remotes", quietly = TRUE)) {
+#  install.packages("remotes")
+#}
+## Install a specific version of 'vctrs'
+#remotes::install_version("vctrs", version = "0.6.4")
 
-# Now, try loading the 'sparklyr' package again
-#install.packages("vctrs")
 
 
